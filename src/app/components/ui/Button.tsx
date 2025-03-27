9018f94fe13adf2c4b6773c41953c03a10b131a3
Brian Deng <69863366+br-iscool@users.noreply.github.com>
2025-03-27 17:59:21 -0600

    feat: finish play ui components

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -8,10 +8,10 @@ export default function PlayButton({ text }: PlayButtonProps) {
     return (
         <>
             <div className="flex text-white items-center justify-center">
-                <div className="card relative flex items-center justify-center rounded-2xl overflow-hidden px-5 py-2.5 bg-gray-700 transition-all duration-300 ease-[ease]">
-                    <h1 className="relative font-medium">
+                <div className="card relative flex items-center justify-center rounded-2xl overflow-hidden bg-gray-700 transition-all duration-300 ease-[ease]">
+                    <button className="relative px-5 py-2.5 font-medium cursor-pointer">
                         { text }
-                    </h1>
+                    </button>
                 </div>
             </div>
         </>
--- src/app/components/ui/play/PlayInput.tsx
+++ src/app/components/ui/play/PlayInput.tsx
@@ -1,5 +1,11 @@
 export default function PlayInput() {
     return (
-        <></>
+        <>
+            <div className="relative p-2">
+                <label htmlFor="play-input" className="block pb-2 text-sm font-medium">Enter your answer: </label>
+                <input type="text" id="play-input" className="block w-full p-4 rounded-lg caret-white bg-gray-700 focus:border-gray-400 focus:ring-gray-400" />
+            </div>
+        
+        </>
     )
 }
