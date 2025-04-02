80fbb81f2d1114a95e216c9609949dd0a88c1a34
Brian Deng <69863366+br-iscool@users.noreply.github.com>
2025-04-01 21:39:48 -0600

    feat: add about infos

---
+++ src/TODO.md
@@ -0,0 +1,5 @@
+### To do:
+
+- Finish play components (input, reader output, etc.)
+- Finish about section (add text)
+- Create LLM with sklearn and implement using axios / fastapi
--- src/app/components/pages/About.tsx
+++ src/app/components/pages/About.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function About() {
 	return (
 		<div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
@@ -5,21 +7,27 @@ export default function About() {
 			<div className="grid pt-8 text-left border-t border-gray-950/5 md:gap-16 dark:border-white/20 md:grid-cols-2">
 				<div>
 					<div className="mb-10">
-						<h3 className="flex items-center mb-4 text-lg font-medium">What do you mean by "Figma assets"?</h3>
+						<h3 className="items-center mb-4 text-lg font-medium">What is Quiz Reader?</h3>
 						<p className="text-gray-500 dark:text-gray-400">
-							You will have access to download the full Figma project including all of the pages, the components, responsive pages, and also the icons, illustrations, and images included in the screens.
+							Quiz Reader is an online quizbowl reader and judge, similar to other sites like {""}
+							<Link href={"https://protobowl.com"} className="underline" target="_blank">Protobowl</Link>
+							{""} and {""}
+							<Link href={"https://qbreader.org"} className="underline" target="_blank">QBReader</Link>
+							.
 						</p>
 					</div>
 					<div className="mb-10">
-						<h3 className="flex items-center mb-4 text-lg font-medium">What does "lifetime access" exactly mean?</h3>
-						<p className="text-gray-500 dark:text-gray-400">Once you have purchased either the design, code, or both packages, you will have access to all of the future updates based on the roadmap, free of charge.</p>
+						<h3 className="items-center mb-4 text-lg font-medium">How does the AI judge questions accurately?</h3>
+						<p className="text-gray-500 dark:text-gray-400">We compare your answers to an intelligent automated database containing synonyms, alternative phrasings, and other accepted answers.</p>
 					</div>
 					<div className="mb-10">
-						<h3 className="flex items-center mb-4 text-lg font-medium">How does support work?</h3>
-						<p className="text-gray-500 dark:text-gray-400">We're aware of the importance of well qualified support, that is why we decided that support will only be provided by the authors that actually worked on this project.</p>{" "}
+						<h3 className="items-center mb-4 text-lg font-medium">How did you build Quiz Reader?</h3>
+						<p className="text-gray-500 dark:text-gray-400">The front-end was built using Next.js, Tailwind, React Icons, while the back-end was built using a combination of Typescript and Python.</p>
+						<p className="text-gray-500 dark:text-gray-400 pt-1.5">Feel free to contribute to the code on <Link href={"https://github.com/br-iscool/quizreader"} className="underline text-gray-500 dark:text-gray-400" target="_blank">Github.</Link></p>
+						
 					</div>
 					<div className="mb-10">
-						<h3 className="flex items-center mb-4 text-lg font-medium">I want to build more than one project. Is that allowed?</h3>
+						<h3 className="items-center mb-4 text-lg font-medium">I want to build more than one project. Is that allowed?</h3>
 						<p className="text-gray-500 dark:text-gray-400">
 							You can use Windster for an unlimited amount of projects, whether it's a personal website, a SaaS app, or a website for a client. As long as you don't build a product that will directly compete with Windster either as a UI
 							kit, theme, or template, it's fine.
@@ -28,7 +36,7 @@ export default function About() {
 				</div>
 				<div>
 					<div className="mb-10">
-						<h3 className="flex items-center mb-4 text-lg font-medium">What does "free updates" include?</h3>
+						<h3 className="items-center mb-4 text-lg font-medium">What does "free updates" include?</h3>
 						<p className="text-gray-500 dark:text-gray-400">
 							The free updates that will be provided is based on the{" "}
 							<a href="#" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">
@@ -38,7 +46,7 @@ export default function About() {
 						</p>
 					</div>
 					<div className="mb-10">
-						<h3 className="flex items-center mb-4 text-lg font-medium">What does the free version include?</h3>
+						<h3 className="items-center mb-4 text-lg font-medium">What does the free version include?</h3>
 						<p className="text-gray-500 dark:text-gray-400">
 							The{" "}
 							<a href="#" className="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline">
@@ -49,14 +57,14 @@ export default function About() {
 						<p className="text-gray-500 dark:text-gray-400">You can use this version for any purposes, because it is open-source under the MIT license.</p>
 					</div>
 					<div className="mb-10">
-						<h3 className="flex items-center mb-4 text-lg font-medium">What is the difference between Windster and Tailwind UI?</h3>
+						<h3 className="items-center mb-4 text-lg font-medium">What is the difference between Windster and Tailwind UI?</h3>
 						<p className="text-gray-500 dark:text-gray-400">
 							Although both Windster and Tailwind UI are built for integration with Tailwind CSS, the main difference is in the design, the pages, the extra components and UI elements that Windster includes.
 						</p>
 						<p className="text-gray-500 dark:text-gray-400">Additionally, Windster is a project that is still in development, and later it will include both the application, marketing, and e-commerce UI interfaces.</p>
 					</div>
 					<div className="mb-10">
-						<h3 className="flex items-center mb-4 text-lg font-medium">Can I use Windster in open-source projects?</h3>
+						<h3 className="items-center mb-4 text-lg font-medium">Can I use Windster in open-source projects?</h3>
 						<p className="text-gray-500 dark:text-gray-400">
 							Generally, it is accepted to use Windster in open-source projects, as long as it is not a UI library, a theme, a template, a page-builder that would be considered as an alternative to Windster itself.
 						</p>
